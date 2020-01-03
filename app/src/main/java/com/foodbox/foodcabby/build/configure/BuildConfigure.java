eac24c6288965705d786f7a04d757e213fad22ca
takiddine <takiddine.job@gmail.com>
2020-01-03 11:59:23 +0000

    first commit

---
+++ app/src/main/java/com/foodbox/foodcabby/build/configure/BuildConfigure.java
@@ -0,0 +1,26 @@
+package com.foodbox.foodcabby.build.configure;
+
+/**
+ * Created by Tamil on 9/22/2017.
+ */
+
+public class BuildConfigure {
+
+
+    /*   Dev Mode*/
+//    public static String BASE_URL = "http://phplaravel-258426-869731.cloudwaysapps.com/";
+    public static String BASE_URL = "https://foodcabby.de/";
+    public static String CLIENT_SECRET = "IXKpta3n8DWBCVMHNBvgNmzPmEB7uuXdAZeUbTVh";
+    public static String CLIENT_ID = "2";
+
+    //Pubnub for Chat
+    public static String PUBNUB_PUBLISH_KEY = "pub-c-1abe81c1-3aa7-4d55-ab0f-bb3a0fe0ecbc";
+    public static String PUBNUB_SUBSCRIBE_KEY = "sub-c-cd6611e2-24b8-11ea-9e12-76e5f2bf83fc";
+    public static String PUBNUB_CHANNEL_NAME = "food";
+
+    //Stripe for card payment
+    public static String STRIPE_PK = "pk_test_39kly6aEfUEfvMpRnN6BnxLb";
+
+    public static final String PAYPAL_CLIENT_ID = "AYeIrLhaOnnK6Fg3NqUbxAN7sF1oyNzTlXKrWs_SS-SNYrlyerOkIwREIyV\u0000xsKPWy-c 5oFlYJlJtDv";
+
+}
